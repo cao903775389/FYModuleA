986d943233473cd275870a07886a1e068599ca65
caofengyang <caofengyang@igengmei.com>
2020-04-28 20:33:42 +0800

    update

--- upload.sh
+++ upload.sh
@@ -10,8 +10,8 @@ if [ "$ConflicCount" -gt 0 ] ; then
    exit 1
 fi
 
-Repo="DUSpecs"
-RepoList=("DUSpecs" "DUSpecs_test" "DUSpecs_gray")
+Repo="FYModuleSpec"
+RepoList=("FYModuleSpec")
 getRepo() {
     echo -e "\n"
     length=${#RepoList[@]}
